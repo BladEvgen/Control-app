c7e17b7d4c0feca479a3b981f7e3a5e259636308
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-18 15:05:30 +0500

    start project

---
+++ backend/monitoring_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ShopAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "monitoring_app"
+    verbose_name = "Система мониторинга"
