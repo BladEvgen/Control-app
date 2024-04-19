120f208cc0cedfc5a1b9b7112db6d5139efd0f53
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-19 18:54:01 +0500

    updated upload_file as class now

--- backend/monitoring_app/views.py
+++ backend/monitoring_app/views.py
@@ -1,9 +1,11 @@
-from drf_yasg import openapi
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.views.generic import (
+    View,
+)
 from rest_framework import status
 from openpyxl import load_workbook
-from django.contrib.auth.models import User
 from rest_framework.response import Response
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 
@@ -74,77 +76,22 @@ def user_register(request):
     )
 
 
-@swagger_auto_schema(
-    method="POST",
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        required=["file"],
-        properties={
-            "file": openapi.Schema(
-                type=openapi.TYPE_STRING,
-                format="binary",
-                description="Файл XLSX для загрузки",
-            )
-        },
-    ),
-    responses={
-        200: "Успешная загрузка файла",
-        400: "Неверный формат файла или ошибка при обработке",
-        405: "Метод не разрешен",
-    },
-)
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def upload_file(request):
-    """
-    Загружает файл XLSX, содержащий данные об отделах, и обрабатывает его.
+class UploadFileView(View):
+    template_name = "upload_file.html"
 
-    **Разрешения:**
-        - Требуется авторизованный пользователь (класс разрешений IsAuthenticated)
+    def get(self, request, *args, **kwargs):
+        categories = models.FileCategory.objects.all()
+        context = {"categories": categories}
+        return render(request, self.template_name, context=context)
 
-    **Запрос:**
-        - Метод: POST
-        - Данные: Мультипарт форма данных с единственным ключом файла под названием "file", содержащим данные XLSX.
+    def post(self, request, *args, **kwargs):
+        file_path = request.FILES.get("file")
 
-    **Ответ:**
-        - При успехе (статус=200):
-            - JSON-ответ с сообщением: "Файл XLSX успешно загружен!"
-        - При ошибке (статус=400):
-            - JSON-ответ с сообщением об ошибке, указывающим причину сбоя.
-                - Возможные сообщения об ошибках:
-                    - "Неверный формат файла." (Неверный формат файла)
-                    - "Ошибка при обработке файла: {details исключения}" (Ошибка обработки файла)
-        - При недопустимом методе (статус=405):
-            - JSON-ответ с сообщением об ошибке: "Метод не разрешен." (Метод не разрешен)
-
-    **Функциональность:**
-        1. Проверяет, является ли метод запроса POST.
-        2. Извлекает загруженный файл из request.FILES.get("file").
-        3.  Проверяет формат файла:
-            - Проверяет, соответствует ли тип контента "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" (XLSX).
-        4. Обрабатывает допустимый файл XLSX:
-            - Загружает книгу с помощью load_workbook.
-            - Удаляет первые две строки (обязательно: в них стоят заголовки файла).
-            - Сортирует оставшиеся строки по значению первого столбца в порядке убывания.
-            - Перебирает отсортированные строки и обновляет значения ячеек на рабочем листе.
-            - Сохраняет обновленную книгу обратно в загруженный файл.
-            - Перебирает строки (начиная с 1-й, если учитывать что прошлые 2 были удалены):
-                - Извлекает идентификатор, имя родительского отдела и имя дочернего отдела.
-                - Использует get_or_create для эффективного создания родительских отделов, если они не существуют.
-                - Создает дочерние отделы с извлеченными данными и присваивает их соответствующему родительскому отделу.
-                - Сохраняет дочерний отдел.
-        5. Возвращает успешный ответ с сообщением при успешной обработке.
-        6. Возвращает ошибку с подробным сообщением в случае любых исключений или ошибок проверки.
-    """
-    if request.method == "POST":
-        file = request.FILES.get("file")
-        if (
-            file
-            and file.content_type
-            == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        ):
+        category_slug = request.POST.get("category")
+        print(category_slug)
+        if file_path and file_path.name.endswith(".xlsx") and category_slug:
             try:
-                wb = load_workbook(file)
+                wb = load_workbook(file_path)
                 ws = wb.active
 
                 ws.delete_rows(1, 2)
@@ -156,39 +103,40 @@ def upload_file(request):
                     for col_idx, cell in enumerate(row, start=1):
                         ws.cell(row=idx, column=col_idx, value=cell.value)
 
-                wb.save(file)
+                wb.save(file_path)
 
-                for row in ws.iter_rows(min_row=3):
-                    parent_department_id = int(row[2].value)
-                    parent_department_name = row[3].value
-                    child_department_name = row[1].value
+                if category_slug == "departments":
+                    for row in ws.iter_rows(min_row=3):
+                        parent_department_id = int(row[2].value)
+                        parent_department_name = row[3].value
+                        child_department_name = row[1].value
 
-                    parent_department, _ = (
-                        models.ParentDepartment.objects.get_or_create(
-                            id=parent_department_id, name=parent_department_name
+                        parent_department, _ = (
+                            models.ParentDepartment.objects.get_or_create(
+                                id=parent_department_id, name=parent_department_name
+                            )
                         )
-                    )
 
-                    child_department = models.ChildDepartment.objects.create(
-                        name=child_department_name, parent=parent_department
-                    )
+                        child_department = models.ChildDepartment.objects.create(
+                            name=child_department_name, parent=parent_department
+                        )
 
-                    child_department.save()
+                        child_department.save()
+                elif category_slug == "staff":
+                    pass
+                else:
+                    context = {
+                        "error": "Неизвестная категория. Пожалуйста, обратитесь к Администратору."
+                    }
 
-                return Response(
-                    {"message": "Файл XLSX успешно загружен!"},
-                    status=status.HTTP_200_OK,
-                )
-            except Exception as e:
-                return Response(
-                    {"error": f"Ошибка при обработке файла: {e}"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
+                context = {"message": "Файл успешно загружен"}
+                return redirect("uploadFile")
+
+            except Exception as error:
+                context = {"error": f"Ошибка при обработке файла: {str(error)}"}
         else:
-            return Response(
-                {"error": "Неверный формат файла."}, status=status.HTTP_400_BAD_REQUEST
-            )
-    else:
-        return Response(
-            {"error": "Метод не разрешен."}, status=status.HTTP_405_METHOD_NOT_ALLOWED
-        )
+            context = {
+                "error": "Проверьте правильность заполненных данных, или неверный формат файла"
+            }
+
+        return render(request, self.template_name, context=context)
