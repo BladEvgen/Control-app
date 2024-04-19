3cc10ea7c4850adacb62be47a91b9b7e6b2d0023
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-19 18:52:59 +0500

    New model for files

--- backend/monitoring_app/models.py
+++ backend/monitoring_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class FileCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class ParentDepartment(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
