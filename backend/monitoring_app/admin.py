90abda84806a74982b22aeb7f24f2c3c1074b48e
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-21 17:45:30 +0500

    custom admin

--- backend/monitoring_app/admin.py
+++ backend/monitoring_app/admin.py
@@ -1,32 +1,96 @@
-from django.apps import apps
 from django.contrib import admin
-from django.db import models as django_models
+from .models import (
+    Staff,
+    Salary,
+    Position,
+    UserProfile,
+    FileCategory,
+    ChildDepartment,
+    ParentDepartment,
+)
 
 admin.site.site_header = "Панель управления"
 admin.site.index_title = "Администрирование сайта"
 admin.site.site_title = "Администрирование"
 
-exclude_models = []
 
-for model in apps.get_models():
-    if model not in exclude_models and model not in admin.site._registry:
-        model_name = model.__name__
-        admin_class_name = f"{model_name}Admin"
-        admin_class = type(
-            admin_class_name,
-            (admin.ModelAdmin,),
-            {
-                "list_display": [field.name for field in model._meta.fields],
-                "list_filter": [
-                    field.name
-                    for field in model._meta.fields
-                    if isinstance(field, django_models.CharField)
-                ],
-                "search_fields": [
-                    field.name
-                    for field in model._meta.fields
-                    if isinstance(field, django_models.CharField)
-                ],
-            },
-        )
-        admin.site.register(model, admin_class)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "is_banned", "phonenumber", "address")
+    list_filter = ("is_banned",)
+    search_fields = ("user__username", "phonenumber", "address")
+    ordering = (
+        "user__username",
+        "-is_banned",
+    )
+    list_editable = ("is_banned",)
+
+
+@admin.register(FileCategory)
+class FileCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+
+
+@admin.register(ParentDepartment)
+class ParentDepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "date_of_creation")
+    search_fields = ("name",)
+    ordering = (
+        "name",
+        "date_of_creation",
+    )
+
+
+@admin.register(ChildDepartment)
+class ChildDepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent", "date_of_creation")
+    search_fields = ("name", "parent__name")
+    ordering = (
+        "name",
+        "-date_of_creation",
+    )
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name", "rate")
+    search_fields = ("name",)
+    ordering = ("-rate", "name")
+    list_editable = ("rate",)
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = (
+        "pin",
+        "surname",
+        "name",
+        "department",
+        "display_positions",
+        "avatar",
+    )
+    list_filter = ("department", "positions")
+    search_fields = ("surname", "name", "department__name")
+    filter_horizontal = ("positions",)
+    actions = ["clear_avatars"]
+
+    def clear_avatars(self, request, queryset):
+        for staff_member in queryset:
+            staff_member.avatar = None
+            staff_member.save()
+
+    clear_avatars.short_description = "Очистить фото сотрудника"
+
+    def display_positions(self, obj):
+        return ", ".join([position.name for position in obj.positions.all()])
+
+    display_positions.short_description = "Должности"
+
+
+@admin.register(Salary)
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = ("staff", "clean_salary", "dirty_salary", "total_salary")
+    search_fields = ("staff__surname", "staff__name")
+    list_filter = ("staff__department",)
+    readonly_fields = ("dirty_salary", "total_salary")
