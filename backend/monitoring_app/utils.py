0b046c576a3557e210c9eb5cbc09e0359ec114d2
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-04-23 15:34:25 +0500

    Optimize Using Bulk create

--- backend/monitoring_app/utils.py
+++ backend/monitoring_app/utils.py
@@ -4,13 +4,23 @@ import requests
 
 from django.conf import settings
 from monitoring_app import models
+from django.db import transaction
 from concurrent.futures import ThreadPoolExecutor
 
 
-def get_attendance(pin: str):
-    session = requests.Session()
+def get_attendance_data(pin: str):
+    """
+    Получает данные о посещаемости сотрудника за предыдущий день.
 
-    prev_date = datetime.datetime.now() - datetime.timedelta(days=10)
+    Args:
+        pin (str): Идентификационный номер сотрудника (PIN-код).
+
+    Returns:
+        list: Список словарей, содержащих информацию о каждом проходе
+              сотрудника через систему контроля доступа за указанный день.
+              Если произошла ошибка, возвращает пустой список.
+    """
+    prev_date = datetime.datetime.now() - datetime.timedelta(days=1)
     eventTime_first = prev_date.strftime("%Y-%m-%d 00:00:00")
     eventTime_last = prev_date.strftime("%Y-%m-%d 23:59:59")
     access_token = settings.API_KEY
@@ -24,65 +34,77 @@ def get_attendance(pin: str):
             "startDate": eventTime_first,
             "access_token": access_token,
         }
-        response = session.get(
+        response = requests.get(
             settings.API_URL + "/api/transaction/listAttTransaction",
             params=params,
         )
         response.raise_for_status()
         response_json = response.json()
-        data: list = response_json.get("data", [])
-        staff = models.Staff.objects.get(pin=pin)
-
-        if data:
-            first_event_time = data[-1]["eventTime"]
-
-            if len(data) == 1:
-                last_event_time = first_event_time
-            else:
-                last_event_time = data[0]["eventTime"]
-
-            staff_attendance = models.StaffAttendance.objects.create(staff=staff)
-
-            if staff_attendance:
-                staff_attendance.first_in = first_event_time
-                staff_attendance.last_out = last_event_time
-                staff_attendance.date_at = prev_date + datetime.timedelta(
-                    days=1
-                )  #! Debug  fill with test data
-                staff_attendance.save()
-        else:
-            staff_attendance: models.StaffAttendance = (
-                models.StaffAttendance.objects.create(staff=staff)
-            )
-            if staff_attendance:
-                staff_attendance.save()
+        return response_json.get("data", [])
     except Exception as e:
         print(f"Error, Pin: {pin}, Error: {e}")
+        return []
 
 
 def get_all_attendance():
+    """
+    Получает данные о посещаемости всех сотрудников за текущий день
+    и сохраняет их в базе данных.
+
+    Выполняет запрос данных о посещаемости для каждого сотрудника
+    параллельно с помощью ThreadPoolExecutor. Затем для каждого
+    сотрудника извлекает первое и последнее время прохода
+    (если данные о посещаемости имеются) и сохраняет их
+    в модели StaffAttendance.
+
+    Returns:
+        None
+    """
     pins = models.Staff.objects.values_list("pin", flat=True)
+    attendance_data = {}
+
     with ThreadPoolExecutor(max_workers=6) as executor:
-        executor.map(get_attendance, pins)
+        for pin, data in zip(pins, executor.map(get_attendance_data, pins)):
+            attendance_data[pin] = data
+
+    with transaction.atomic():
+        for pin, data in attendance_data.items():
+            staff = models.Staff.objects.get(pin=pin)
+            if data:
+                first_event_time = data[-1]["eventTime"]
+                last_event_time = (
+                    data[0]["eventTime"] if len(data) > 1 else first_event_time
+                )
+            else:
+                first_event_time = None
+                last_event_time = None
+
+            staff_attendance = models.StaffAttendance.objects.create(
+                staff=staff,
+                first_in=first_event_time,
+                last_out=last_event_time,
+                date_at=datetime.date.today(),
+            )
+            staff_attendance.save()
 
 
 def password_check(password: str) -> bool:
     """
-    Checks if a password meets the system's complexity requirements.
+    Проверяет, соответствует ли пароль требованиям сложности системы.
 
-    This function validates a password string based on the following criteria:
+    Эта функция проверяет строку пароля на основе следующих критериев:
 
-    - Minimum length of 8 characters
-    - Contains at least one uppercase letter (A-Z)
-    - Contains at least one lowercase letter (a-z)
-    - Contains at least one digit (0-9)
-    - Contains at least one special character from the following set: #?!@$%^&*-
+        - Минимальная длина 8 символов.
+        - Содержит хотя бы одну заглавную букву (A-Z)
+        - Содержит хотя бы одну строчную букву (a-z)
+        - Содержит хотя бы одну цифру (0-9)
+        - Содержит хотя бы один специальный символ из следующего набора: #?!@$%^&*-
 
     Args:
-        password (str): The password string to be validated.
+        пароль (str): строка пароля, которую необходимо проверить.
 
     Returns:
-        bool: True if the password meets all complexity requirements, False otherwise.
+        bool: True, если пароль соответствует всем требованиям сложности, в противном случае — False.
     """
     return bool(
         re.match(
