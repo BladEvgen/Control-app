69576c803c3db98da68e21fb172e8e4ec5adebd3
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-05-02 13:01:32 +0500

    redesign footer

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -2,28 +2,32 @@ const FooterComponent = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="absolute bottom-0 left-0 right-0 py-3 bg-gray-200">
-      <div className="container mx-auto">
-        <ul className="flex justify-center border-b pb-3 mb-3">
-          <li className="mr-6">
-            <a href="/" className="text-gray-600 hover:text-gray-800">
-              Home
-            </a>
-          </li>
-          <li className="mr-6">
-            <a href="#" className="text-gray-600 hover:text-gray-800">
-              FAQs
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="text-gray-600 hover:text-gray-800">
-              About Us
-            </a>
-          </li>
-        </ul>
-        <p className="text-center text-gray-600">©{currentYear} Company, Inc</p>
-      </div>
-    </footer>
+    <div className="relative max-h-screen">
+      <footer className="sticky bottom-0 left-0 right-0 py-3 bg-gray-200">
+        <div className="container mx-auto">
+          <ul className="flex justify-center border-b pb-3 mb-3">
+            <li className="mr-6">
+              <a href="/" className="text-gray-600 hover:text-gray-800">
+                Home
+              </a>
+            </li>
+            <li className="mr-6">
+              <a href="#" className="text-gray-600 hover:text-gray-800">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="text-gray-600 hover:text-gray-800">
+                About Us
+              </a>
+            </li>
+          </ul>
+          <p className="text-center text-gray-600">
+            ©{currentYear} Company, Inc
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
