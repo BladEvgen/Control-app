62d1448ce14c812cef160dbece992699924557a6
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-02 01:18:55 +0500

    update button design

--- frontend/src/pages/StaffDetail.tsx
+++ frontend/src/pages/StaffDetail.tsx
@@ -293,7 +293,7 @@ const StaffDetail = () => {
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Всего минут
+                      Всего времени (Ч:М)
                     </th>
                   </tr>
                 </thead>
@@ -306,11 +306,11 @@ const StaffDetail = () => {
             </div>
 
             <button
-              className="fixed bottom-4 left-4 bg-white rounded-full p-3 hover:bg-green-300 shadow-md z-10 focus:outline-none"
+              className="fixed bottom-4 left-4 bg-green-500 text-white rounded-full px-4 py-2  hover:bg-green-700 shadow-md z-10 focus:outline-none"
               onClick={navigateToChildDepartment}
             >
               <span role="img" aria-label="Back" className="text-xl">
-                🔙
+                ◀
               </span>
             </button>
           </div>
