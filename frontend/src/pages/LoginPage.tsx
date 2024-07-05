f5f019e772f933eca7a757426bd6116a9e2dab10
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-06 01:31:38 +0500

    redesign login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import axiosInstance from "../api";
 import Cookies from "js-cookie";
 import { useNavigate } from "../RouterUtils";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async () => {
@@ -34,32 +36,46 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
-      <input
-        className="w-full px-4 py-2 mb-4 text-lg bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        name="username"
-        placeholder="Username"
-        type="text"
-      />
-      <input
-        className="w-full px-4 py-2 mb-4 text-lg bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        name="password"
-        placeholder="Password"
-        type="password"
-        onKeyPress={handleKeyPress}
-      />
-      <button
-        className="w-full px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-        onClick={handleSubmit}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <div
+        className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg md:max-w-lg lg:max-w-xl xl:max-w-2xl"
+        style={{ marginTop: "-25%" }}
       >
-        Login
-      </button>
+        <h2 className="text-3xl font-bold text-center text-gray-800">Login</h2>
+        <input
+          className="w-full px-4 py-2 mb-4 text-lg bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          name="username"
+          placeholder="Username"
+          type="text"
+        />
+        <div className="relative w-full">
+          <input
+            className="w-full px-4 py-2 mb-4 text-lg bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            placeholder="Password"
+            type={showPassword ? "text" : "password"}
+            onKeyPress={handleKeyPress}
+          />
+          <button
+            type="button"
+            className="absolute inset-y-0 right-0 flex items-center justify-center mb-3 px-3 text-gray-600"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <FaEyeSlash size={24} /> : <FaEye size={24} />}
+          </button>
+        </div>
+        <button
+          className="w-full px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          onClick={handleSubmit}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 };
-
 export default LoginPage;
