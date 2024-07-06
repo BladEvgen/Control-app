221b24da9922ec7286f288ecef8865108948d3af
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-06 14:11:59 +0500

    new design
    Now added dropdown and auto hide for it

--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -1,22 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import { Link, useNavigate } from "../RouterUtils";
 import Cookies from "js-cookie";
-import { apiUrl } from "../../apiConfig";
 import axiosInstance from "../api";
+import { apiUrl } from "../../apiConfig";
+import {
+  FaUser,
+  FaSignOutAlt,
+  FaUpload,
+  FaBars,
+  FaTimes,
+  FaAngleDown,
+} from "react-icons/fa";
 
 const HeaderComponent = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [username, setUsername] = useState("");
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [username, setUsername] = useState<string>("");
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const profileButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     checkAuthentication();
   }, []);
 
-  const checkAuthentication = async () => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) &&
+        !profileButtonRef.current?.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const checkAuthentication = useCallback(async () => {
     const accessToken = Cookies.get("access_token");
     const refreshToken = Cookies.get("refresh_token");
 
@@ -35,14 +63,13 @@ const HeaderComponent = () => {
         navigate("/login");
       }
     }
-  };
+  }, [navigate, location.pathname]);
 
   const handleLogout = () => {
     Cookies.remove("access_token");
     Cookies.remove("refresh_token");
     Cookies.remove("sessionid");
     setIsAuthenticated(false);
-
     window.location.reload();
   };
 
@@ -50,70 +77,75 @@ const HeaderComponent = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <header className="bg-[#123075]">
-      <nav className="flex items-center justify-between flex-wrap p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <Link to="/">
-            <span className="font-semibold text-xl tracking-tight">
-              Control Staff App
-            </span>
-          </Link>
+    <header className="bg-blue-900 text-white shadow-md">
+      <nav className="container mx-auto p-4 flex items-center justify-between">
+        <Link to="/" className="text-2xl font-bold">
+          Control Staff App
+        </Link>
+        <div className="lg:hidden">
+          <button className="text-2xl" onClick={toggleMobileMenu}>
+            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
         </div>
-        <div className="w-full block lg:flex lg:items-center lg:w-auto">
+        <div
+          className={`lg:flex items-center ${
+            isMobileMenuOpen ? "block" : "hidden"
+          } lg:block`}
+        >
           {isAuthenticated ? (
-            <div className="text-sm lg:flex-grow">
-              <div className="relative">
-                <button
-                  className="bg-gray-800 text-white font-bold py-2 px-4 rounded lg:inline-flex lg:w-auto"
-                  onClick={toggleDropdown}
+            <div className="relative mt-4 lg:mt-0 lg:ml-4">
+              <button
+                ref={profileButtonRef}
+                className="flex items-center bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded relative"
+                onClick={toggleDropdown}
+              >
+                {username} <FaAngleDown className="ml-2" />
+              </button>
+              {isDropdownOpen && (
+                <div
+                  ref={dropdownRef}
+                  className="absolute right-0 mt-2 w-48 bg-white text-gray-800 rounded-md shadow-lg z-50"
+                  onMouseLeave={handleMouseLeave}
                 >
-                  {username}
-                  <svg
-                    className="h-5 w-5 inline-block ml-2 lg:hidden"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M9.293 13.707a1 1 0 01-1.414 0l-3-3a1 1 0 111.414-1.414L8 11.586V3a1 1 0 112 0v8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-                {isDropdownOpen && (
-                  <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg lg:absolute lg:right-0">
-                    <button
-                      onClick={handleLogout}
-                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left"
-                    >
-                      Logout
+                  <Link to="/profile">
+                    <button className="flex items-center px-4 py-3 hover:bg-gray-200 w-full text-left border-gray-300 rounded">
+                      <FaUser className="mr-2 text-blue-500" />
+                      Profile
                     </button>
-                  </div>
-                )}
-              </div>
-            </div>
-          ) : (
-            <div>
-              {location.pathname !== "/login" && (
-                <Link to="/login">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Login
+                  </Link>
+                  <a href={`${apiUrl}/upload`}>
+                    <button className="flex items-center px-4 py-3  hover:bg-gray-200  w-full text-left border-gray-300">
+                      <FaUpload className="mr-2 text-green-500 hover:text-green-700" />
+                      Upload
+                    </button>
+                  </a>
+                  <button
+                    onClick={handleLogout}
+                    className="flex items-center px-4 py-3 hover:bg-gray-200 w-full text-left border-t text-red-500"
+                  >
+                    <FaSignOutAlt className="mr-2" />
+                    Logout
                   </button>
-                </Link>
+                </div>
               )}
             </div>
-          )}
-          {isAuthenticated && (
-            <div className="flex mt-4 lg:mt-0 lg:ml-4">
-              <a
-                href={`${apiUrl}/upload`}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded lg:inline-flex lg:w-auto"
-              >
-                Upload Data
-              </a>
-            </div>
+          ) : (
+            location.pathname !== "/login" && (
+              <Link to="/login" className="mt-4 lg:mt-0 lg:ml-4">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Login
+                </button>
+              </Link>
+            )
           )}
         </div>
       </nav>
