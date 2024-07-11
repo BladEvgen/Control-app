3223480448789a1fae8068de5c992b392d3b67e5
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-12 00:09:20 +0500

    redesign page

--- frontend/src/pages/StaffDetail.tsx
+++ frontend/src/pages/StaffDetail.tsx
@@ -111,14 +111,23 @@ const StaffDetail = () => {
     return `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
   };
 
-  const renderAttendanceRow = (date: string, data: AttendanceData) => {
+  const renderAttendanceRow = (
+    date: string,
+    data: AttendanceData,
+    isFirst: boolean,
+    isLast: boolean
+  ) => {
     const isWeekend = data.is_weekend;
     const hasInOut = data.first_in && data.last_out;
+
+    const rowClassNames = `px-6 py-3 whitespace-nowrap ${
+      isFirst ? "rounded-t-lg" : ""
+    } ${isLast ? "rounded-b-lg" : ""}`;
 
     if (!hasInOut && !isWeekend) {
       return (
         <tr key={date} className="bg-red-100">
-          <td colSpan={5} className="px-6 py-4 whitespace-nowrap">
+          <td colSpan={5} className={rowClassNames}>
             {date}: Нет данных
           </td>
         </tr>
@@ -126,7 +135,7 @@ const StaffDetail = () => {
     } else if (!hasInOut && isWeekend) {
       return (
         <tr key={date} className="bg-yellow-100">
-          <td colSpan={5} className="px-6 py-4 whitespace-nowrap">
+          <td colSpan={5} className={rowClassNames}>
             {date}: Выходной день
           </td>
         </tr>
@@ -134,15 +143,15 @@ const StaffDetail = () => {
     } else {
       return (
         <tr key={date} className={isWeekend ? "bg-green-100" : ""}>
-          <td className="px-6 py-3 whitespace-nowrap">{date}</td>
-          <td className="px-6 py-3 whitespace-nowrap">
+          <td className={rowClassNames}>{date}</td>
+          <td className={rowClassNames}>
             {data.first_in ? formatDate(data.first_in) : "Нет данных"}
           </td>
-          <td className="px-6 py-3 whitespace-nowrap">
+          <td className={rowClassNames}>
             {data.last_out ? formatDate(data.last_out) : "Нет данных"}
           </td>
-          <td className="px-6 py-3 whitespace-nowrap">{data.percent_day}%</td>
-          <td className="px-6 py-3 whitespace-nowrap">
+          <td className={rowClassNames}>{data.percent_day}%</td>
+          <td className={rowClassNames}>
             {data.total_minutes
               ? formatMinutes(data.total_minutes)
               : "Нет данных"}
@@ -205,10 +214,10 @@ const StaffDetail = () => {
   ) {
     const percent_for_period = staffData.percent_for_period;
 
-    if (percent_for_period > 80) {
-      if (percent_for_period >= 95) {
+    if (percent_for_period > 105) {
+      if (percent_for_period >= 115) {
         bonusPercentage = 20;
-      } else if (percent_for_period >= 85) {
+      } else if (percent_for_period >= 110) {
         bonusPercentage = 15;
       } else {
         bonusPercentage = 10;
@@ -239,48 +248,47 @@ const StaffDetail = () => {
       ) : (
         staffData && (
           <div>
-            <div className="flex flex-col sm:flex-row items-center relative">
-              <img
-                src={`${apiUrl}${staffData.avatar}`}
-                alt="Avatar"
-                className="w-32 h-32 rounded-full mb-4 sm:mr-6"
-              />
-
-              <div className="flex-grow">
-                <p className="text-lg">
-                  <strong>ФИО:</strong> {staffData.surname} {staffData.name}
-                </p>
-                <p className="text-lg">
-                  <strong>Отдел:</strong> {staffData.department}
-                </p>
-                <p className="text-lg">
-                  <strong>Должность:</strong>{" "}
-                  {staffData.positions?.join(", ") || "Нет данных"}
-                </p>
-                <p className="text-lg">
-                  <strong>Зарплата:</strong> {formatNumber(staffData.salary)}
-                </p>
-                <p className="text-lg">
-                  <strong>Процент за выбранный период:</strong>{" "}
-                  {staffData.percent_for_period} %
-                </p>
-                <p className="text-lg">
-                  <TooltipText
-                    text={`${startDate ? startDate : oneMonthStartDate} - ${
-                      endDate ? endDate : oneMonthEndDate
-                    }`}
-                    daysCount={Object.keys(staffData.attendance).length}
-                  />
-                </p>
+            <div className="relative mb-6">
+              <div className="flex flex-col items-center sm:flex-row sm:items-start sm:space-x-6">
+                <img
+                  src={`${apiUrl}${staffData.avatar}`}
+                  alt="Avatar"
+                  className="w-24 h-24 sm:w-32 sm:h-32 rounded-full mb-4 sm:mb-0 shadow-md"
+                />
+                <div className="text-center sm:text-left">
+                  <p className="text-lg font-semibold mb-1">
+                    <strong>ФИО:</strong> {staffData.surname} {staffData.name}
+                  </p>
+                  <p className="text-lg font-semibold mb-1">
+                    <strong>Отдел:</strong> {staffData.department}
+                  </p>
+                  <p className="text-lg font-semibold mb-1">
+                    <strong>Должность:</strong>{" "}
+                    {staffData.positions?.join(", ") || "Нет данных"}
+                  </p>
+                  <p className="text-lg font-semibold mb-1">
+                    <strong>Зарплата:</strong> {formatNumber(staffData.salary)}
+                  </p>
+                  <p className="text-lg font-semibold mb-1">
+                    <strong>Процент за выбранный период:</strong>{" "}
+                    {staffData.percent_for_period} %
+                  </p>
+                  <p className="text-lg mb-2">
+                    <TooltipText
+                      text={`${startDate ? startDate : oneMonthStartDate} - ${
+                        endDate ? endDate : oneMonthEndDate
+                      }`}
+                      daysCount={Object.keys(staffData.attendance).length}
+                    />
+                  </p>
+                </div>
+                <button
+                  className="absolute left-0 top-0 sm:left-8 sm:top-8 lg:-left-14 lg:-top-4 bg-green-500 text-white rounded-full p-2 sm:p-3 hover:bg-green-700 shadow-md z-10 focus:outline-none transition-transform transform hover:scale-105 hidden lg:block"
+                  onClick={navigateToChildDepartment}
+                >
+                  <FaChevronLeft className="text-lg sm:text-xl" />
+                </button>
               </div>
-              <button
-                className="absolute -left-8 -top-4 bg-green-500 text-white rounded-full px-4 py-3 hover:bg-green-700 shadow-md z-10 focus:outline-none hidden sm:block"
-                onClick={navigateToChildDepartment}
-              >
-                <span role="img" aria-label="Back" className="text-xl">
-                  <FaChevronLeft />
-                </span>
-              </button>
             </div>
 
             {bonusPercentage > 0 && (
@@ -292,29 +300,29 @@ const StaffDetail = () => {
                 )
               </p>
             )}
-            <h2 className="text-2xl font-bold mt-8 mb-4 flex justify-center">
+            <h2 className="text-2xl font-bold mt-8 mb-4 text-center">
               Посещаемость
             </h2>
 
-            <div className="flex justify-center items-center space-x-4 text-sm text-gray-700 mt-4">
-              <div className="flex items-center">
-                <div className="w-4 h-4 rounded-full bg-yellow-300 mr-2"></div>
+            <div className="flex flex-wrap justify-center gap-4 text-sm text-gray-700 mt-4">
+              <div className="flex items-center space-x-2">
+                <div className="w-4 h-4 rounded-full bg-yellow-300"></div>
                 <span className="font-semibold">Выходной день</span>
               </div>
-              <div className="flex items-center">
-                <div className="w-4 h-4 rounded-full bg-red-300 mr-2"></div>
+              <div className="flex items-center space-x-2">
+                <div className="w-4 h-4 rounded-full bg-red-300"></div>
                 <span className="font-semibold">Работник отсутствовал</span>
               </div>
-              <div className="flex items-center">
-                <div className="w-4 h-4 rounded-full bg-green-300 mr-2"></div>
+              <div className="flex items-center space-x-2">
+                <div className="w-4 h-4 rounded-full bg-green-300"></div>
                 <span className="font-semibold">
                   Работник был на работе в выходной
                 </span>
               </div>
             </div>
 
-            <div className="mb-4 flex flex-wrap justify-center sm:justify-between">
-              <div className="mb-2 sm:mb-0">
+            <div className="mb-4 flex flex-col sm:flex-row sm:space-x-4 justify-center sm:justify-between items-center sm:items-end">
+              <div className="mb-2 sm:mb-0 w-full sm:w-auto">
                 <label
                   htmlFor="startDate"
                   className="block text-sm font-medium text-gray-700 mb-1"
@@ -326,12 +334,12 @@ const StaffDetail = () => {
                   id="startDate"
                   value={startDate}
                   onChange={handleStartDateChange}
-                  className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full sm:w-auto"
+                  className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full sm:w-auto transition-colors duration-300"
                   placeholder="Дата начала"
                 />
               </div>
               {startDate && (
-                <div className="mt-2 sm:mt-0">
+                <div className="mt-2 sm:mt-0 w-full sm:w-auto">
                   <label
                     htmlFor="endDate"
                     className="block text-sm font-medium text-gray-700 mb-1"
@@ -343,7 +351,7 @@ const StaffDetail = () => {
                     id="endDate"
                     value={endDate}
                     onChange={handleEndDateChange}
-                    className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full sm:w-auto"
+                    className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full sm:w-auto transition-colors duration-300"
                     placeholder="Дата окончания"
                   />
                 </div>
@@ -390,13 +398,19 @@ const StaffDetail = () => {
                 <tbody className="bg-white divide-y divide-gray-200">
                   {Object.entries(staffData.attendance)
                     .reverse()
-                    .map(([date, data]) => renderAttendanceRow(date, data))}
+                    .map(([date, data], index, array) =>
+                      renderAttendanceRow(
+                        date,
+                        data,
+                        index === 0,
+                        index === array.length - 1
+                      )
+                    )}
                 </tbody>
               </table>
             </div>
-
             <button
-              className="fixed bottom-4 -left-0 bg-green-500 text-white rounded-full px-4 py-4 hover:bg-green-700 shadow-md z-10 focus:outline-none sm:hidden"
+              className="fixed bottom-4 left-4 bg-green-500 text-white rounded-full px-4 py-4 hover:bg-green-700 shadow-md z-10 focus:outline-none sm:block md:block lg:hidden"
               onClick={navigateToChildDepartment}
             >
               <span role="img" aria-label="Back" className="text-xl">
