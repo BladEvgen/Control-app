0c37a8b5e0bd0849396ac4865d4f6f30045202c7
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-21 16:51:09 +0500

    Update FooterComponent.tsx

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -4,26 +4,35 @@ const FooterComponent = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="py-3 bg-gray-200 mt-auto">
+    <footer className="py-4 bg-footer-light text-text-dark dark:bg-background-dark dark:text-text-light mt-auto">
       <div className="container mx-auto">
-        <div className="flex justify-center border-b pb-3 mb-3">
+        <div className="flex justify-center border-b border-text-dark dark:border-text-light pb-3 mb-3">
           <div className="mr-6">
-            <Link to="/" className="text-gray-600 hover:text-gray-800">
+            <Link
+              to="/"
+              className="text-text-dark hover:text-primary dark:text-text-light dark:hover:text-primary-light"
+            >
               Home
             </Link>
           </div>
           <div className="mr-6">
-            <Link to="/" className="text-gray-600 hover:text-gray-800">
+            <Link
+              to="/"
+              className="text-text-dark hover:text-primary dark:text-text-light dark:hover:text-primary-light"
+            >
               FAQs
             </Link>
           </div>
           <div>
-            <Link to="/" className="text-gray-600 hover:text-gray-800">
+            <Link
+              to="/"
+              className="text-text-dark hover:text-primary dark:text-text-light dark:hover:text-primary-light"
+            >
               About Us
             </Link>
           </div>
         </div>
-        <p className="text-center text-gray-600">
+        <p className="text-center text-text-dark dark:text-text-light">
           © <span id="currentYear">{currentYear}</span> Company, Inc
         </p>
       </div>
