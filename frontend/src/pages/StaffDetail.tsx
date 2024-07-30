a09548bc559b29f4337ee55dbd8c2c3f4692b15a
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-07-30 23:35:04 +0500

    Notification if error

--- frontend/src/pages/StaffDetail.tsx
+++ frontend/src/pages/StaffDetail.tsx
@@ -7,7 +7,7 @@ import { StaffData } from "../schemas/IData";
 import { FaChevronLeft } from "react-icons/fa";
 import { FiInfo } from "react-icons/fi";
 import { formatDepartmentName } from "../utils/utils";
-
+import Notification from "../components/Notification";
 import DateForm from "./DateForm";
 import AttendanceTable from "./AttendanceTable";
 import { formatDateRu, formatNumber, declensionDays } from "../utils/utils";
@@ -41,6 +41,7 @@ const StaffDetail = () => {
   const [loading, setLoading] = useState(true);
   const [oneMonthStartDate, setOneMonthStartDate] = useState<string>("");
   const [oneMonthEndDate, setOneMonthEndDate] = useState<string>("");
+  const [showNotification, setShowNotification] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -67,8 +68,17 @@ const StaffDetail = () => {
         });
         setStaffData(res.data);
         setLoading(false);
-      } catch (error) {
-        console.error(`Error fetching staff data: ${error}`);
+      } catch (error: any) {
+        if (error.response && error.response.status === 404) {
+          setTimeout(() => {
+            setShowNotification(true);
+            setLoading(false);
+            setError("Сотрудник не найден");
+          }, 3000);
+        } else {
+          console.error(`Error fetching staff data: ${error}`);
+          setLoading(false);
+        }
       }
     };
 
@@ -106,8 +116,17 @@ const StaffDetail = () => {
           percent_for_period: res.data.percent_for_period,
         });
       }
-    } catch (error) {
-      console.error(`Error fetching one month data: ${error}`);
+    } catch (error: any) {
+      if (error.response && error.response.status === 404) {
+        setTimeout(() => {
+          setShowNotification(true);
+          setLoading(false);
+          setError("Сотрудник не найден");
+        }, 3000);
+      } else {
+        console.error(`Error fetching one month data: ${error}`);
+        setLoading(false);
+      }
     }
   };
 
@@ -183,119 +202,128 @@ const StaffDetail = () => {
           <div className="loader"></div>
         </div>
       ) : (
-        staffData && (
-          <div>
-            <div className="relative mb-6">
-              <div className="flex flex-col items-center sm:flex-row sm:items-start sm:space-x-6">
-                <img
-                  src={`${apiUrl}${staffData.avatar}`}
-                  alt="Avatar"
-                  className="w-24 h-24 sm:w-32 sm:h-32 rounded-full mb-4 sm:mb-0 shadow-md"
-                />
-                <div className="text-center sm:text-left">
-                  {staffData.surname && staffData.name && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>ФИО:</strong> {staffData.surname} {staffData.name}
-                    </p>
-                  )}
-                  {staffData.department && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>Отдел:</strong>{" "}
-                      {formatDepartmentName(staffData.department)}
-                    </p>
-                  )}
-                  {staffData.positions && staffData.positions.length > 0 && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>Должность:</strong>{" "}
-                      {staffData.positions.join(", ")}
-                    </p>
-                  )}
-                  {staffData.salary !== null && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>Зарплата:</strong>{" "}
-                      {formatNumber(staffData.salary)}
-                    </p>
-                  )}
-                  {staffData.contract_type && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>Тип занятости:</strong>{" "}
-                      {getContractTypeLabel(staffData.contract_type)}
-                    </p>
-                  )}
-                  {staffData.percent_for_period !== undefined && (
-                    <p className="text-lg font-semibold mb-1">
-                      <strong>Процент за выбранный период:</strong>{" "}
-                      {staffData.percent_for_period} %
-                    </p>
-                  )}
-                  {Object.keys(staffData.attendance).length > 0 && (
-                    <p className="text-lg mb-2">
-                      <TooltipText
-                        text={`${startDate ? startDate : oneMonthStartDate} - ${
-                          endDate ? endDate : oneMonthEndDate
-                        }`}
-                        daysCount={Object.keys(staffData.attendance).length}
-                      />
-                    </p>
-                  )}
+        <>
+          {showNotification && error ? (
+            <Notification message={error} type="error" />
+          ) : (
+            staffData && (
+              <div>
+                <div className="relative mb-6">
+                  <div className="flex flex-col items-center sm:flex-row sm:items-start sm:space-x-6">
+                    <img
+                      src={`${apiUrl}${staffData.avatar}`}
+                      alt="Avatar"
+                      className="w-24 h-24 sm:w-32 sm:h-32 rounded-full mb-4 sm:mb-0 shadow-md"
+                    />
+                    <div className="text-center sm:text-left">
+                      {staffData.surname && staffData.name && (
+                        <p className="text-lg font-semibold mb-1">
+                          <strong>ФИО:</strong> {staffData.surname}{" "}
+                          {staffData.name}
+                        </p>
+                      )}
+                      {staffData.department && (
+                        <p className="text-lg font-semibold mb-1">
+                          <strong>Отдел:</strong>{" "}
+                          {formatDepartmentName(staffData.department)}
+                        </p>
+                      )}
+                      {staffData.positions &&
+                        staffData.positions.length > 0 && (
+                          <p className="text-lg font-semibold mb-1">
+                            <strong>Должность:</strong>{" "}
+                            {staffData.positions.join(", ")}
+                          </p>
+                        )}
+                      {staffData.salary !== null && (
+                        <p className="text-lg font-semibold mb-1">
+                          <strong>Зарплата:</strong>{" "}
+                          {formatNumber(staffData.salary)}
+                        </p>
+                      )}
+                      {staffData.contract_type && (
+                        <p className="text-lg font-semibold mb-1">
+                          <strong>Тип занятости:</strong>{" "}
+                          {getContractTypeLabel(staffData.contract_type)}
+                        </p>
+                      )}
+                      {staffData.percent_for_period !== undefined && (
+                        <p className="text-lg font-semibold mb-1">
+                          <strong>Процент за выбранный период:</strong>{" "}
+                          {staffData.percent_for_period} %
+                        </p>
+                      )}
+                      {Object.keys(staffData.attendance).length > 0 && (
+                        <p className="text-lg mb-2">
+                          <TooltipText
+                            text={`${
+                              startDate ? startDate : oneMonthStartDate
+                            } - ${endDate ? endDate : oneMonthEndDate}`}
+                            daysCount={Object.keys(staffData.attendance).length}
+                          />
+                        </p>
+                      )}
+                    </div>
+                    <button
+                      className="absolute left-0 top-0 sm:left-8 sm:top-8 lg:-left-14 lg:-top-4 bg-green-500 text-white rounded-full p-2 sm:p-3 hover:bg-green-700 shadow-md z-10 focus:outline-none transition-transform transform hover:scale-105 hidden lg:block"
+                      onClick={navigateToChildDepartment}
+                    >
+                      <FaChevronLeft className="text-lg sm:text-xl" />
+                    </button>
+                  </div>
                 </div>
+
+                {bonusPercentage > 0 && (
+                  <p className="text-lg text-green-600 dark:text-green-400 mt-4">
+                    Сотрудник может получить премию в размере {bonusPercentage}%
+                    (
+                    {formatNumber(
+                      ((staffData.salary ?? 0) * bonusPercentage) / 100
+                    )}
+                    )
+                  </p>
+                )}
+                <h2 className="text-2xl font-bold mt-8 mb-4 text-center">
+                  Посещаемость
+                </h2>
+
+                <div className="flex flex-wrap justify-center gap-4 text-sm text-gray-700 dark:text-gray-400 mt-4">
+                  <div className="flex items-center space-x-2">
+                    <div className="w-4 h-4 rounded-full bg-yellow-400 dark:bg-yellow-500"></div>
+                    <span className="font-semibold">Выходной день</span>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <div className="w-4 h-4 rounded-full bg-red-400 dark:bg-red-500"></div>
+                    <span className="font-semibold">Работник отсутствовал</span>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <div className="w-4 h-4 rounded-full bg-green-400 dark:bg-green-500"></div>
+                    <span className="font-semibold">
+                      Работник был на работе в выходной
+                    </span>
+                  </div>
+                </div>
+
+                <DateForm
+                  startDate={startDate}
+                  endDate={endDate}
+                  handleStartDateChange={handleStartDateChange}
+                  handleEndDateChange={handleEndDateChange}
+                  error={error}
+                />
+
+                <AttendanceTable attendance={staffData.attendance} />
+
                 <button
-                  className="absolute left-0 top-0 sm:left-8 sm:top-8 lg:-left-14 lg:-top-4 bg-green-500 text-white rounded-full p-2 sm:p-3 hover:bg-green-700 shadow-md z-10 focus:outline-none transition-transform transform hover:scale-105 hidden lg:block"
+                  className="fixed bottom-4 left-4 bg-green-500 text-white rounded-full px-4 py-4 hover:bg-green-700 shadow-md z-10 focus:outline-none sm:block md:block lg:hidden"
                   onClick={navigateToChildDepartment}
                 >
-                  <FaChevronLeft className="text-lg sm:text-xl" />
+                  <FaChevronLeft className="text-xl" />
                 </button>
               </div>
-            </div>
-
-            {bonusPercentage > 0 && (
-              <p className="text-lg text-green-600 dark:text-green-400 mt-4">
-                Сотрудник может получить премию в размере {bonusPercentage}% (
-                {formatNumber(
-                  ((staffData.salary ?? 0) * bonusPercentage) / 100
-                )}
-                )
-              </p>
-            )}
-            <h2 className="text-2xl font-bold mt-8 mb-4 text-center">
-              Посещаемость
-            </h2>
-
-            <div className="flex flex-wrap justify-center gap-4 text-sm text-gray-700 dark:text-gray-400 mt-4">
-              <div className="flex items-center space-x-2">
-                <div className="w-4 h-4 rounded-full bg-yellow-400 dark:bg-yellow-500"></div>
-                <span className="font-semibold">Выходной день</span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <div className="w-4 h-4 rounded-full bg-red-400 dark:bg-red-500"></div>
-                <span className="font-semibold">Работник отсутствовал</span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <div className="w-4 h-4 rounded-full bg-green-400 dark:bg-green-500"></div>
-                <span className="font-semibold">
-                  Работник был на работе в выходной
-                </span>
-              </div>
-            </div>
-
-            <DateForm
-              startDate={startDate}
-              endDate={endDate}
-              handleStartDateChange={handleStartDateChange}
-              handleEndDateChange={handleEndDateChange}
-              error={error}
-            />
-
-            <AttendanceTable attendance={staffData.attendance} />
-
-            <button
-              className="fixed bottom-4 left-4 bg-green-500 text-white rounded-full px-4 py-4 hover:bg-green-700 shadow-md z-10 focus:outline-none sm:block md:block lg:hidden"
-              onClick={navigateToChildDepartment}
-            >
-              <FaChevronLeft className="text-xl" />
-            </button>
-          </div>
-        )
+            )
+          )}
+        </>
       )}
     </div>
   );
