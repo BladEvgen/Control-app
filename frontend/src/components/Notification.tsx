d995155c67c2e199e015225c92c9f98b14a59317
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-08-19 17:04:33 +0300

    notification now can use links

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "../RouterUtils";
+import { FaHome } from "react-icons/fa";
 
 interface NotificationProps {
   message: string;
   type: "warning" | "error";
+  link?: string;
 }
 
-const Notification: React.FC<NotificationProps> = ({ message, type }) => {
+const Notification: React.FC<NotificationProps> = ({ message, type, link }) => {
   const bgColor =
     type === "warning"
       ? "bg-yellow-100 dark:bg-yellow-900"
@@ -20,7 +23,7 @@ const Notification: React.FC<NotificationProps> = ({ message, type }) => {
     type === "warning" ? "animate-bounce" : "animate-pulse";
 
   return (
-    <div className="flex justify-center items-center h-screen px-4">
+    <div className="flex flex-col justify-center items-center h-screen px-4">
       <div
         className={`${bgColor} ${borderColor} ${textColor} px-8 py-6 rounded-lg shadow-lg transition transform duration-500 ease-in-out ${animationClass} max-w-lg w-full mx-auto`}
         role="alert"
@@ -29,6 +32,15 @@ const Notification: React.FC<NotificationProps> = ({ message, type }) => {
           {type === "warning" ? "Предупреждение!" : "Ошибка!"}
         </p>
         <p className="text-lg md:text-xl">{message}</p>
+        {link && (
+          <Link
+            to={link}
+            className="mt-6 bg-gray-800 text-white px-6 py-3 rounded-lg shadow-md hover:bg-gray-900 transition transform hover:-translate-y-1 hover:scale-105 flex items-center justify-center"
+          >
+            <FaHome className="mr-2" />
+            Вернуться на главную
+          </Link>
+        )}
       </div>
     </div>
   );
