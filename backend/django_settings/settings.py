f8c4ad1f90afb7be4525f22a98fe770ce0c73d17
bladevgen <ekozlov00@mail.ru>
2024-08-19 16:21:15 +0500

    hosts update

--- backend/django_settings/settings.py
+++ backend/django_settings/settings.py
@@ -30,7 +30,7 @@ LOGOUT_URL = "/logout/"
 
 
 #! SET TO DOMAIN
-ALLOWED_HOSTS = ["control.krmu.edu.kz", "dot.medkrmu.kz"]
+ALLOWED_HOSTS = ["control.krmu.edu.kz", "dot.medkrmu.kz", "localhost", "127.0.0.1"]
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 МБ
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 МБ
