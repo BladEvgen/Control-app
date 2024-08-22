e6d839ef63752fe49c34a52c7721a9b5a345d55c
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-08-22 19:09:25 +0300

    changed animation

--- frontend/src/pages/ChildDepartmentPage.tsx
+++ frontend/src/pages/ChildDepartmentPage.tsx
@@ -241,7 +241,24 @@ const ChildDepartmentPage = () => {
                 } shadow-md`}
               >
                 {isDownloading ? (
-                  <div className="loader"></div>
+                  <svg
+                    className="animate-spin h-5 w-5 mr-3 text-white"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+                    ></path>
+                  </svg>
                 ) : (
                   <FaDownload className="mr-2" />
                 )}
