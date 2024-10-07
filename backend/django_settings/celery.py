dee1d1d0b5ea646388c51a89cf80cbd9dcf2f9e6
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-10-07 19:27:08 +0500

    Update celery.py

--- backend/django_settings/celery.py
+++ backend/django_settings/celery.py
@@ -6,6 +6,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings.settings')
 
 app = Celery('django_settings')
 
+app.conf.update(
+    broker_url='redis://localhost:6379/0',  
+    result_backend='redis://localhost:6379/0',  
+)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
