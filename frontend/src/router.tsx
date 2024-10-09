807f9e0b0731c42733ed031408563e7da91706a5
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-10-09 16:22:27 +0500

    Revert "lazy import"
    This reverts commit 0c7b18278151c49e46770bb6cb6ce7fbdfbab3fd.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,23 +1,16 @@
 import React, { useEffect, useState, ReactNode } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-const MainPage = React.lazy(() => import("./pages/MainPage.tsx"));
-const HeaderComponent = React.lazy(
-  () => import("./components/HeaderComponent.tsx")
-);
-const FooterComponent = React.lazy(
-  () => import("./components/FooterComponent.tsx")
-);
-const LoginPage = React.lazy(() => import("./pages/LoginPage.tsx"));
-const DepartmentPage = React.lazy(() => import("./pages/DepartmentPage.tsx"));
-const ChildDepartmentPage = React.lazy(
-  () => import("./pages/ChildDepartmentPage.tsx")
-);
-const StaffDetail = React.lazy(() => import("./pages/StaffDetail.tsx"));
-const Dashboard = React.lazy(() => import("./pages/Dashboard.tsx"));
-const MapPage = React.lazy(() => import("./pages/MapDashboard.tsx"));
-
+import MainPage from "./pages/MainPage.tsx";
+import HeaderComponent from "./components/HeaderComponent.tsx";
+import FooterComponent from "./components/FooterComponent.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import DepartmentPage from "./pages/DepartmentPage.tsx";
+import ChildDepartmentPage from "./pages/ChildDepartmentPage.tsx";
+import StaffDetail from "./pages/StaffDetail.tsx";
 import { addPrefix } from "./RouterUtils.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import MapPage from "./pages/MapDashboard.tsx";
 interface LayoutProps {
   children: ReactNode;
 }
