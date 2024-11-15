e8cadec12124d545792f4216c735a1f72d84210c
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-11-15 20:06:23 +0500

    better asgi

--- backend/django_settings/asgi.py
+++ backend/django_settings/asgi.py
@@ -1,16 +1,36 @@
 import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings.settings')
-django.setup()  
+import logging
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import monitoring_app.routing  
+from channels.auth import AuthMiddlewareStack
+from channels.middleware import BaseMiddleware
+from channels.exceptions import DenyConnection
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
-        monitoring_app.routing.websocket_urlpatterns
-    ),
-})
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings.settings')
+
+logger = logging.getLogger("django")
+
+django_asgi_app = get_asgi_application()
+
+
+class LoggingMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        logger.info(f"Request scope: {scope}")
+        try:
+            return await super().__call__(scope, receive, send)
+        except Exception as e:
+            logger.error("Error processing WebSocket request: %s", e)
+            raise DenyConnection()
+
+
+import monitoring_app.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": LoggingMiddleware(
+            AuthMiddlewareStack(URLRouter(monitoring_app.routing.websocket_urlpatterns))
+        ),
+    }
+)
