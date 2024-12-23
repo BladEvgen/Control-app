34385d4c7e0efd04863badd57f6b78fd5bb824cc
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-12-23 18:37:07 +0500

    stabilize signal

--- backend/monitoring_app/signals.py
+++ backend/monitoring_app/signals.py
@@ -1,18 +1,23 @@
-from django.db.models.signals import post_save
+import re
+import logging
 from django.dispatch import receiver
 from .models import LessonAttendance
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-import logging
+from django.db.models.signals import post_save
 
 logger = logging.getLogger(__name__)
+
+
+def sanitize_group_name(name):
+    return re.sub(r"[^a-zA-Z0-9_\-\.]", "_", name)[:100]
 
 
 @receiver(post_save, sender=LessonAttendance)
 def send_new_photo(sender, instance, created, **kwargs):
     if created:
         channel_layer = get_channel_layer()
-        group_name = f"photos_{instance.date_at}"
+        group_name = sanitize_group_name(f"photos_{instance.date_at.isoformat()}")
         async_to_sync(channel_layer.group_send)(
             group_name, {"type": "new_photo", "attendance_id": instance.id}
         )
