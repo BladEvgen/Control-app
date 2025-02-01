4f3e63bf4a4ccff3dc036b684d1699104f293911
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-02-01 14:47:31 +0500

    update design

--- frontend/src/pages/ChildDepartmentPage.tsx
+++ frontend/src/pages/ChildDepartmentPage.tsx
@@ -72,7 +72,6 @@ const ChildDepartmentPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       dispatch(new BaseAction(BaseAction.SET_LOADING, true));
-
       try {
         const res = await axiosInstance.get(
           `${apiUrl}/api/child_department/${id}/`
@@ -85,7 +84,6 @@ const ChildDepartmentPage = () => {
         );
       }
     };
-
     fetchData();
   }, [id]);
 
@@ -120,10 +118,7 @@ const ChildDepartmentPage = () => {
       const response = await axiosInstance.get(
         `${apiUrl}/api/download/${id}/`,
         {
-          params: {
-            startDate,
-            endDate,
-          },
+          params: { startDate, endDate },
           responseType: "blob",
           timeout: 600000,
         }
@@ -137,7 +132,6 @@ const ChildDepartmentPage = () => {
           ? data.child_department.name.replace(/\s/g, "_")
           : data.child_department.child_id.toString();
       }
-
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
@@ -155,17 +149,39 @@ const ChildDepartmentPage = () => {
 
   const isDownloadDisabled = !startDate || !endDate;
 
+  const containerVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  };
+  const buttonVariants = {
+    hover: { scale: 1.1, transition: { duration: 0.2 } },
+    tap: { scale: 0.95, transition: { duration: 0.1 } },
+  };
+  const tableVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+  };
+  const rowVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8 dark:text-white">
+    <motion.div
+      className="mx-auto px-4 py-8 dark:text-white max-w-full md:max-w-[70vw]"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       {isLoading ? (
         <LoaderComponent />
       ) : error ? (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
           <FaTimesCircle className="text-red-500 text-5xl mb-4" />
           <p className="text-xl text-red-600 dark:text-red-400">{error}</p>
           <Link
             to="/"
-            className="mt-6 px-4 py-2 bg-yellow-500 text-white text-lg rounded-lg shadow-md hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 transition-colors duration-300 ease-in-out"
+            className="mt-6 px-4 py-2 bg-yellow-500 dark:bg-yellow-700 text-white text-lg rounded-lg shadow-md hover:bg-yellow-600 transition-colors duration-300 ease-in-out"
           >
             Вернуться на главную
           </Link>
@@ -177,45 +193,79 @@ const ChildDepartmentPage = () => {
               formatDepartmentName(data.child_department.name)}
           </h1>
 
-          <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-            <div className="flex justify-center space-x-4 mt-4 md:mt-0">
+          {/* Верхний блок с кнопками – виден только на устройствах md и выше */}
+          <div className="hidden md:flex items-end justify-between mb-6">
+            <div className="flex space-x-4">
               <Link
                 to="/"
-                className="flex items-center px-3 py-2 bg-yellow-500 text-white text-lg rounded-lg shadow-md hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 transition-colors duration-300 ease-in-out"
+                className="flex items-center px-4 py-2 bg-yellow-500 dark:bg-yellow-700 text-white text-lg rounded-lg shadow-md hover:bg-yellow-600 transition-colors duration-300 ease-in-out"
               >
                 <FaHome className="mr-2" />
                 <span className="font-semibold">На главную</span>
               </Link>
               <button
                 onClick={navigateToChildDepartment}
-                className="flex items-center px-3 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800 transition-colors duration-300 ease-in-out"
+                className="flex items-center px-4 py-2 bg-blue-500 text-white text-lg rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300 ease-in-out"
               >
                 <FaArrowLeft className="mr-2" />
                 <span>Вернуться назад</span>
               </button>
             </div>
+            {/* Скрываем эту кнопку на ПК */}
+            <button
+              onClick={handleDownload}
+              disabled={isDownloadDisabled || isDownloading}
+              className="flex items-center px-4 py-2 mt-2 bg-green-500 hover:bg-green-600 text-white text-lg rounded-lg shadow-md transition-colors duration-300 ease-in-out md:hidden"
+            >
+              {isDownloading ? (
+                <svg
+                  className="animate-spin h-5 w-5 mr-2 text-white"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+                  ></path>
+                </svg>
+              ) : (
+                <FaDownload className="mr-2" />
+              )}
+              {isDownloading ? "Загрузка" : "Скачать"}
+            </button>
           </div>
 
-          <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
-            <div className="flex flex-col mb-4 md:mb-0">
-              <label
-                htmlFor="startDate"
-                className="block mb-2 font-medium text-gray-200 dark:text-gray-400"
-              >
-                Дата начала:
-              </label>
-              <input
-                type="date"
-                id="startDate"
-                value={startDate}
-                onChange={handleStartDateChange}
-                className="border border-gray-300 px-4 py-2 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:border-gray-600"
-              />
+          {/* Блок с датами и кнопкой скачать (для устройств ниже md) */}
+          <div className="flex flex-col sm:flex-row md:items-end items-center sm:space-x-4 mb-4 px-2 ml">
+            <div className="flex flex-col sm:flex-row sm:items-end w-full sm:w-auto space-y-4 sm:space-y-0">
+              <div className="w-full sm:w-40">
+                <label
+                  htmlFor="startDate"
+                  className="block mb-1 font-medium text-gray-200 dark:text-gray-400"
+                >
+                  Дата начала:
+                </label>
+                <input
+                  type="date"
+                  id="startDate"
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  className="border border-gray-300 px-3 py-2 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+                />
+              </div>
             </div>
-            <div className="flex flex-col mb-4 md:mb-0">
+            <div className="w-full sm:w-40 mt-4 sm:mt-0">
               <label
                 htmlFor="endDate"
-                className="block mb-2 font-medium text-gray-200 dark:text-gray-400"
+                className="block mb-1 font-medium text-gray-200 dark:text-gray-400"
               >
                 Дата конца:
               </label>
@@ -224,22 +274,18 @@ const ChildDepartmentPage = () => {
                 id="endDate"
                 value={endDate}
                 onChange={handleEndDateChange}
-                className="border border-gray-300 px-4 py-2 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+                className="border border-gray-300 px-3 py-2 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:border-gray-600"
               />
             </div>
-            <div className="flex flex-col md:flex-row md:items-center">
+            <div className="w-full sm:w-auto mt-4 sm:mt-0 md:self-end">
               <button
                 onClick={handleDownload}
                 disabled={isDownloadDisabled || isDownloading}
-                className={`flex items-center justify-center w-full md:w-auto px-4 py-2 rounded-lg text-white mt-4 md:mt-7 ${
-                  isDownloadDisabled || isDownloading
-                    ? "bg-gray-400 cursor-not-allowed dark:bg-gray-600"
-                    : "bg-green-500 hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700"
-                } shadow-md`}
+                className="w-full sm:w-auto px-4 py-2 bg-green-500 hover:bg-green-600 text-white text-lg rounded-lg shadow-md transition-colors duration-300 ease-in-out"
               >
                 {isDownloading ? (
                   <svg
-                    className="animate-spin h-5 w-5 mr-3 text-white"
+                    className="animate-spin h-5 w-5 inline mr-2 text-white"
                     viewBox="0 0 24 24"
                   >
                     <circle
@@ -257,29 +303,29 @@ const ChildDepartmentPage = () => {
                     ></path>
                   </svg>
                 ) : (
-                  <FaDownload className="mr-2" />
+                  <FaDownload className="inline mr-2" />
                 )}
                 {isDownloading ? "Загрузка" : "Скачать"}
               </button>
-              {showWaitMessage && (
-                <motion.div
-                  animate={{ opacity: [1, 0.1, 1] }}
-                  transition={{
-                    duration: 2,
-                    repeat: Infinity,
-                    repeatType: "loop",
-                    ease: "easeInOut",
-                  }}
-                  className="mt-2 md:mt-6 md:ml-4 p-3 bg-red-100 text-red-600 text-sm rounded-lg shadow-md dark:bg-red-900 dark:text-red-200"
-                >
-                  Загрузка может занять некоторое время, пожалуйста,
-                  подождите...
-                </motion.div>
-              )}
             </div>
+            {showWaitMessage && (
+              <motion.div
+                animate={{ opacity: [1, 0.1, 1] }}
+                transition={{
+                  duration: 2,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  ease: "easeInOut",
+                }}
+                className="mt-2 p-3 bg-red-100 text-red-600 text-sm rounded-lg shadow-md dark:bg-red-900 dark:text-red-200"
+              >
+                Загрузка может занять некоторое время, пожалуйста, подождите...
+              </motion.div>
+            )}
           </div>
 
-          <div className="mb-6">
+          {/* Инпут для поиска – во всю ширину */}
+          <div className="mb-6 px-2">
             <input
               type="text"
               placeholder="Поиск по ФИО"
@@ -289,12 +335,12 @@ const ChildDepartmentPage = () => {
             />
           </div>
 
-          <p className="text-gray-300 mb-4 dark:text-gray-400">
+          <p className="text-gray-300 mb-4 px-2 dark:text-gray-400">
             <strong>Количество сотрудников:</strong> {data?.staff_count}
           </p>
 
-          {/* Для мобильных устройств */}
-          <div className="block md:hidden space-y-4">
+          {/* Мобильная версия карточек */}
+          <div className="block md:hidden space-y-2">
             {data?.staff_data &&
               Object.entries(data.staff_data)
                 .filter(([, staff]) =>
@@ -303,9 +349,9 @@ const ChildDepartmentPage = () => {
                 .map(([pin, staff]) => (
                   <div
                     key={pin}
-                    className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md"
+                    className="w-full p-6 rounded-lg shadow-md bg-white dark:bg-gray-800"
                   >
-                    <div className="flex justify-between items-center mb-2">
+                    <div className="flex justify-between items-center mb-3">
                       <Link
                         to={`/staffDetail/${pin}`}
                         className="text-indigo-600 hover:text-indigo-800 dark:text-indigo-400 dark:hover:text-indigo-600 font-semibold text-lg"
@@ -324,7 +370,7 @@ const ChildDepartmentPage = () => {
                         />
                       )}
                     </div>
-                    <p className="text-gray-600 dark:text-gray-400">
+                    <p className="text-gray-600 dark:text-gray-400 text-base">
                       <strong>Должность: </strong>
                       {staff.positions.length > 2
                         ? `${staff.positions[0]}, ... (ещё ${
@@ -332,7 +378,7 @@ const ChildDepartmentPage = () => {
                           })`
                         : staff.positions.join(", ")}
                     </p>
-                    <p className="text-gray-600 dark:text-gray-400">
+                    <p className="text-gray-600 dark:text-gray-400 text-base mt-1">
                       <strong>Дата создания: </strong>
                       {new Date(staff.date_of_creation).toLocaleDateString()}
                     </p>
@@ -340,26 +386,31 @@ const ChildDepartmentPage = () => {
                 ))}
           </div>
 
-          {/* Для планшетов и ПК */}
-          <div className="hidden md:block overflow-x-auto rounded-lg shadow-lg bg-white dark:bg-gray-900">
-            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-              <thead className="bg-gray-50 dark:bg-gray-800">
+          {/* Версия для планшетов и ПК с анимацией таблицы */}
+          <motion.div
+            variants={tableVariants}
+            initial="hidden"
+            animate="visible"
+            className="hidden md:block overflow-x-auto rounded-lg shadow-lg bg-white dark:bg-gray-900 px-2"
+          >
+            <table className="w-full divide-y divide-gray-200 dark:divide-gray-700">
+              <thead className="w-full rounded-t-lg">
                 <tr>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                  <th className="px-6 py-4 text-left text-sm font-semibold text-blue-700 dark:text-blue-300 uppercase tracking-wider">
                     ФИО
                   </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                  <th className="px-6 py-4 text-left text-sm font-semibold text-blue-700 dark:text-blue-300 uppercase tracking-wider">
                     Должность
                   </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                  <th className="px-6 py-4 text-left text-sm font-semibold text-blue-700 dark:text-blue-300 uppercase tracking-wider">
                     Дата создания
                   </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                  <th className="px-6 py-4 text-left text-sm font-semibold text-blue-700 dark:text-blue-300 uppercase tracking-wider">
                     Avatar
                   </th>
                 </tr>
               </thead>
-              <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-900 dark:divide-gray-700">
+              <tbody className="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
                 {data?.staff_data &&
                   Object.entries(data.staff_data)
                     .filter(([, staff]) =>
@@ -368,11 +419,15 @@ const ChildDepartmentPage = () => {
                       )
                     )
                     .map(([pin, staff]) => (
-                      <tr key={pin}>
+                      <motion.tr
+                        key={pin}
+                        variants={rowVariants}
+                        className="hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
+                      >
                         <td className="px-6 py-4 whitespace-nowrap">
                           <Link
                             to={`/staffDetail/${pin}`}
-                            className="text-indigo-600 hover:text-indigo-800 dark:text-indigo-400 dark:hover:text-indigo-600"
+                            className="text-indigo-600 hover:text-indigo-800 dark:text-indigo-400 dark:hover:text-indigo-200"
                           >
                             {staff.FIO}
                           </Link>
@@ -407,14 +462,36 @@ const ChildDepartmentPage = () => {
                             />
                           )}
                         </td>
-                      </tr>
+                      </motion.tr>
                     ))}
               </tbody>
             </table>
-          </div>
+          </motion.div>
         </>
       )}
-    </div>
+
+      {/* Плавающие кнопки для смартфонов (видны только на экранах ниже md) */}
+      <div className="block md:hidden">
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+          whileTap="tap"
+          className="fixed bottom-4 left-4 bg-blue-600 hover:bg-blue-700 text-white rounded-full p-4 shadow-lg z-50 focus:outline-none transition-transform"
+          onClick={navigateToChildDepartment}
+        >
+          <FaArrowLeft size={24} />
+        </motion.button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+          whileTap="tap"
+          onClick={() => navigate("/")}
+          className="fixed bottom-4 right-4 bg-yellow-500 hover:bg-yellow-600 text-white rounded-full p-4 shadow-lg z-50 focus:outline-none transition-transform"
+        >
+          <FaHome size={24} />
+        </motion.button>
+      </div>
+    </motion.div>
   );
 };
 
