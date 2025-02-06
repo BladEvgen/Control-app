a736afccf96302e3ab626291a3e2d807c1718caf
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-02-07 00:18:37 +0500

    Design update

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,15 +1,31 @@
 import { useState, useCallback } from "react";
 import axiosInstance, { setCookie } from "../api";
 import { useNavigate } from "../RouterUtils";
-import { FaEye, FaEyeSlash, FaInfoCircle } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaSignInAlt } from "react-icons/fa";
 import { FaBug } from "react-icons/fa6";
 import { apiUrl } from "../../apiConfig";
+import { motion, AnimatePresence } from "framer-motion";
+
+const errorVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 10 },
+};
+
+const pulseVariants = {
+  initial: { scale: 1 },
+  animate: {
+    scale: [1, 1.05, 1],
+    transition: { duration: 1.5, repeat: Infinity, ease: "easeInOut" },
+  },
+};
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [loginError, setLoginError] = useState("");
+  const [failedAttempts, setFailedAttempts] = useState(0);
 
   const navigate = useNavigate();
 
@@ -17,24 +33,28 @@ const LoginPage = () => {
     const formattedUsername = username.trim().toLowerCase();
 
     try {
-      const res = await axiosInstance.post("/token/", {
-        username: formattedUsername,
-        password,
-      });
+      const res = await axiosInstance.post(
+        "/token/",
+        { username: formattedUsername, password },
+        { skipAuthInterceptor: true }
+      );
 
       setCookie("access_token", res.data.access, { path: "/" });
       setCookie("refresh_token", res.data.refresh, { path: "/" });
       setCookie("username", formattedUsername, { path: "/" });
-
+      setFailedAttempts(0);
       navigate("/");
       window.location.reload();
     } catch (error: any) {
       console.error("Login error:", error);
-      setLoginError("Ошибка входа. Пожалуйста, попробуйте позже.");
+      setLoginError(
+        "Ошибка входа. Проверьте введённые данные или попробуйте позже."
+      );
+      setFailedAttempts((prev) => prev + 1);
+      setTimeout(() => {
+        setLoginError("");
+      }, 5000);
     }
-    setTimeout(() => {
-      setLoginError("");
-    }, 7000);
   }, [username, password, navigate]);
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
@@ -44,66 +64,79 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-footer-light dark:bg-background-dark">
-      <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-b from-primary-dark to-footer-light dark:from-primary-dark dark:to-background-dark py-8 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl p-6 space-y-6 bg-white dark:bg-gray-900 rounded-lg shadow-lg bg-opacity-90">
-          <h2 className="text-3xl font-bold text-center text-dark-blue dark:text-text-light mb-4">
-            Welcome Back!
+    <div className="flex flex-col min-h-screen ">
+      <div className="flex-grow flex flex-col items-center justify-center p-4">
+        <div className="w-full max-w-3xl p-10 bg-white dark:bg-gray-900 rounded-xl shadow-2xl border border-gray-300 relative overflow-hidden">
+          <h2 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-100 mb-6">
+            Добро пожаловать!
           </h2>
-          <p className="text-center text-gray-600 dark:text-gray-400">
-            Please login to your account
+          <p className="text-center text-gray-600 dark:text-gray-300 mb-6">
+            Войдите в свою учётную запись
           </p>
-          <input
-            className="w-full px-4 py-2 mt-4 text-lg bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-light dark:focus:ring-primary transition-all duration-300 ease-in-out"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            name="username"
-            placeholder="Username"
-            type="text"
-          />
-          <div className="relative w-full">
+          <div className="space-y-5">
             <input
-              className="w-full px-4 py-2 mt-4 text-lg bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-light dark:focus:ring-primary pr-10 transition-all duration-300 ease-in-out"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              name="password"
-              placeholder="Password"
-              type={showPassword ? "text" : "password"}
-              onKeyPress={handleKeyPress}
+              className="w-full px-4 py-3 text-lg border dark:bg-gray-800  border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 text-gray-900 dark:text-gray-100"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Логин"
+              type="text"
             />
-            <button
-              type="button"
-              className="absolute top-[1.6rem] right-2 flex items-center justify-center px-3 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <FaEyeSlash size={24} /> : <FaEye size={24} />}
-            </button>
+            <div className="relative">
+              <input
+                className="w-full px-4 py-3 text-lg border dark:bg-gray-800 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 pr-12 transition-all duration-300 text-gray-900 dark:text-gray-100"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Пароль"
+                type={showPassword ? "text" : "password"}
+                onKeyDown={handleKeyPress}
+              />
+              <button
+                type="button"
+                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-600 hover:text-gray-800 transition-colors duration-300"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaEyeSlash className="dark:text-white" size={20} />
+                ) : (
+                  <FaEye className="dark:text-white" size={20} />
+                )}
+              </button>
+            </div>
           </div>
           <button
-            className="w-full px-4 py-2 mt-6 text-lg font-semibold text-white bg-primary dark:bg-primary-dark rounded-md hover:bg-primary-dark dark:hover:bg-darker-blue focus:outline-none focus:bg-primary-dark dark:focus:bg-darker-blue transition-all duration-300 ease-in-out transform hover:scale-105"
+            className="w-full mt-8 px-4 py-3 flex items-center justify-center gap-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 transform hover:scale-105"
             onClick={handleSubmit}
           >
-            Login
+            <FaSignInAlt size={20} />
+            <span>Войти</span>
           </button>
-          <div className="flex items-center justify-between mt-4">
-            <div className="flex items-center text-primary hover:text-blue-600 dark:text-primary-light dark:hover:text-blue-400 transition-colors duration-300 ease-in-out">
-              <FaInfoCircle className="mr-2" />
-              <a
-                className="underline text-lg font-medium"
-                href={`${apiUrl}/password-reset`}
+          <AnimatePresence>
+            {loginError && (
+              <motion.div
+                className="mt-4 flex items-center justify-center px-5 py-3 bg-red-500 text-white rounded-md text-center font-medium text-sm"
+                variants={errorVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
               >
-                Forgot password?
-              </a>
-            </div>
-          </div>
-          {loginError && (
-            <div className="p-4 bg-red-500 dark:bg-red-700 rounded-lg shadow-md flex items-center mt-4">
-              <div className="mr-2">
-                <FaBug className="text-white" size={24} />
-              </div>
-              <p className="text-lg font-semibold text-white">{loginError}</p>
-            </div>
-          )}
+                <FaBug className="inline mr-2" size={20} />
+                <span>{loginError}</span>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <motion.div
+            className="mt-4 text-center"
+            variants={failedAttempts >= 2 ? pulseVariants : {}}
+            initial="initial"
+            animate={failedAttempts >= 2 ? "animate" : ""}
+          >
+            <a
+              className="underline text-blue-600 dark:text-blue-300 hover:text-blue-800 dark:hover:text-blue-200 transition-colors duration-300"
+              href={`${apiUrl}/password-reset`}
+            >
+              Забыли пароль?
+            </a>
+          </motion.div>
         </div>
       </div>
     </div>
