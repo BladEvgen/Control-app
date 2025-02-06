da429dce1006bfb5ab3588bcb53d480de29729e4
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-02-07 00:17:56 +0500

    updated position

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -6,9 +6,15 @@ interface NotificationProps {
   message: string;
   type: "warning" | "error";
   link?: string;
+  linkText?: string;
 }
 
-const Notification: React.FC<NotificationProps> = ({ message, type, link }) => {
+const Notification: React.FC<NotificationProps> = ({
+  message,
+  type,
+  link,
+  linkText,
+}) => {
   const bgColor =
     type === "warning"
       ? "bg-yellow-100 dark:bg-yellow-900"
@@ -19,8 +25,8 @@ const Notification: React.FC<NotificationProps> = ({ message, type, link }) => {
     type === "warning"
       ? "text-yellow-700 dark:text-yellow-300"
       : "text-red-700 dark:text-red-300";
-  const animationClass =
-    type === "warning" ? "animate-bounce" : "animate-pulse";
+
+  const animationClass = type === "warning" ? "animate-bounce" : "animate-once";
 
   return (
     <div className="flex flex-col justify-center items-center h-screen px-4">
@@ -38,7 +44,7 @@ const Notification: React.FC<NotificationProps> = ({ message, type, link }) => {
             className="mt-6 bg-gray-800 text-white px-6 py-3 rounded-lg shadow-md hover:bg-gray-900 transition transform hover:-translate-y-1 hover:scale-105 flex items-center justify-center"
           >
             <FaHome className="mr-2" />
-            Вернуться на главную
+            {linkText ? linkText : "Вернуться на главную"}
           </Link>
         )}
       </div>
